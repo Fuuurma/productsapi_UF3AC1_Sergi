cce84d34521a4785ecb1968923bbe90d0eddb938
fuuurma <sergiformatjer1999@gmail.com>
2024-04-29 19:54:24 +0200

    Mongo Connection

--- config/config.js
+++ config/config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  mongoURI: ""
+  mongoURI:
+    "mongodb+srv://sergiAdmin:HO4ir9sj71xbFtLv@sergicluster.puyw4yo.mongodb.net/?retryWrites=true&w=majority&appName=sergiCluster",
 };
+// mongodb+srv://sergiAdmin:HO4ir9sj71xbFtLv@sergicluster.puyw4yo.mongodb.net/?retryWrites=true&w=majority&appName=sergiCluster
+// sergiAdmin - HO4ir9sj71xbFtLv
+// mongodb+srv://<username>:<password>@sergicluster.puyw4yo.mongodb.net/
